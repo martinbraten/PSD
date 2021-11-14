ff8383e682cff7fad40fc5fbd54b21c93fa776f9
Martin Bråten Stangjordet <stangjordet.df@outlook.com>
2021-11-14 20:28:08 +0100

    uploading code to Git repository first time

---
+++ Random_numbers.py
@@ -0,0 +1,38 @@
+import random
+
+
+class random_num:
+    def __init__(self, amount, minRange, maxRange):
+        self.amount = amount
+        self.minRange = minRange
+        self.maxRange = maxRange
+
+    # Generate x amount of digit in the range.
+    def myrandom(self):
+        mylist = random.sample(range(self.minRange, self.maxRange), self.amount)
+        return mylist
+
+
+# set value
+def setvalue(amount):
+    return amount
+
+
+# set minRange
+def setminRange(minimum):
+    return minimum
+
+
+# set maxRange
+def setmaxRange(maximum):
+    maximum += 1
+    return maximum
+
+
+x = setvalue(int(input('Enter amount of random numbers to return:')))
+y = setminRange(int(input('Enter starting range of random number:')))
+z = setmaxRange(int(input('Enter max number that the random number can be:')))
+
+mycall = random_num(x, y, z)
+
+print(mycall.myrandom(), sep=', ')
